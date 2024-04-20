f0c48a6a4bcc4d47615e29c2cd2a31ddd2059b6e
Sitamrajuvishal <166850111+Sitamrajuvishal@users.noreply.github.com>
2024-04-20 23:38:27 +0530

    Add files via upload

---
+++ java/Day-05_Assignment-05/com/math/operations/Addition.java
@@ -0,0 +1,9 @@
+package com.math.operations;
+
+public class Addition {
+
+	public static double add(double a, double b) {
+		return a+b;
+	}
+
+}
---
+++ java/Day-05_Assignment-05/com/math/operations/Multiplication.java
@@ -0,0 +1,8 @@
+package com.math.operations;
+
+public class Multiplication {
+	
+	public static double multiply(double a, double b) {
+		return a*b;
+	}
+}
